8476305b09967f720aa11470e3f6fe91477b06f6
abh22ishek <abh22ishek@gmail.com>
2018-03-22 18:36:33 +0530

    SignOut added

--- app/src/main/java/abhishek/com/firebasesampleloginauth/MainActivity.java
+++ app/src/main/java/abhishek/com/firebasesampleloginauth/MainActivity.java
@@ -2,19 +2,44 @@ package abhishek.com.firebasesampleloginauth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView userId;
-
+    Button signOut;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userId=findViewById(R.id.userId);
+        signOut=findViewById(R.id.signOut);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try
+                {
+                    FirebaseAuth.getInstance().signOut();
+                    Toast.makeText(MainActivity.this,"sign Out Successful",Toast.LENGTH_SHORT).show();
+                    finish();
+
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this,"Unable to SignOut",Toast.LENGTH_SHORT).show();
+                   e.printStackTrace();
+                }
+
+            }
+        });
 
         if(getIntent().getExtras().getString("user")!=null)
         {
